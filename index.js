bb120bcbfb4e8d66402deea11731e94e143e6864
393Creative <143082004+393Creative@users.noreply.github.com>
2023-10-04 10:22:49 +0800

    idek

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-let firstCard = 10
-let secondCard = 4
+let firstCard = getRandomCard()
+let secondCard = getRandomCard()
+let space = " "
 let cards = [firstCard, secondCard]
 let sum = firstCard + secondCard
 let hasBlackJack = false
@@ -8,6 +9,10 @@ let message = ""
 let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el")
+
+function getRandomCard() {
+    return 5
+}
 
 function startGame() {
     renderGame()
@@ -25,11 +30,19 @@ function renderGame() {
     }
     sumEl.textContent = "Sum: " + sum
     messageEl.textContent = message
-    cardsEl.textContent = "Cards: " + cards[0] + " " + cards[1]
+    cardsEl.textContent = "Cards: " 
+    for(let i = 0; i < cards.length; i++){
+        cardsEl.textContent += cards[i] + "," + space
+    }
 }
 
 function newCard() {
-    let card = 7
+    let card = getRandomCard()
     sum += card
+    cards.push(card)
     renderGame()
+}
+
+function getRandomCard() {
+    
 }
